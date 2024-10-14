1f5dfcb14e083918a178e57655c398d1583fa725
lucarl07 <junitinho2007@gmail.com>
2024-10-14 10:55:52 -0300

    🗑️ remove: Footer component

--- src/components/Footer/index.jsx
+++
@@ -1,9 +0,0 @@
-import Root from "./FooterRoot";
-import ChallengeSrc from "./FooterChallengeSrc";
-import UserSrc from "./FooterUserSrc";
-
-const Footer = {
-  Root, ChallengeSrc, UserSrc
-}
-
-export default Footer;
