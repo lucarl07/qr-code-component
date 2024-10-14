080afcc73971a4710d86275ed83a33d8ec894529
lucarl07 <junitinho2007@gmail.com>
2024-10-14 19:43:11 -0300

    🚀 🐛 Set base as /qr-code-component at vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/qr-code-component",
   plugins: [react()],
   build: { 
     outDir: "build"
