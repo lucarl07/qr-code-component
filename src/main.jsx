894bafef77dff381b96646ee2e51bad7c9746fbd
lucarl07 <junitinho2007@gmail.com>
2024-10-14 07:33:50 -0300

    🚚 chore: Rename src/styles.scss to defaults.scss

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,11 @@
+// Stylesheets:
+import * as bootstrap from "bootstrap";
+import "./defaults.scss";
+
 // Dependencies:
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./pages/App";
-
-// Stylesheets:
-import * as bootstrap from "bootstrap";
-import "./styles.scss";
-import "./index.scss";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
